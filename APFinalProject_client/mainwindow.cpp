a457db86e2112da25ae134755a8723dc8659b01f
nika <you@example.com>
2025-06-26 07:06:16 +0330

    client:general configuration of network such as sending and receiveing data

--- APFinalProject_client/mainwindow.cpp
+++ APFinalProject_client/mainwindow.cpp
@@ -1,14 +1,99 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+
+QTcpSocket* MainWindow::socket = nullptr;
+QString MainWindow::lastReceivedMessage = "hello";
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
+    if(socket)
+    {
+        delete socket;
+    }
+
     delete ui;
 }
+
+void MainWindow::socket_connected()
+{
+    qDebug()<<"connected";
+    ui->connecting_status->setText("connected");
+    sendData("client added");//this shows that senddata is working.
+}
+
+void MainWindow::socket_readyRead()
+{
+    QByteArray data = socket->readAll();
+    lastReceivedMessage = QString::fromUtf8(data);
+    ui->data->setText("data:"+lastReceivedMessage);
+    qDebug()<<"Data: " + data;
+}
+
+void MainWindow::socket_bytesWritten()
+{
+    qDebug()<<"Data was written!";
+}
+
+void MainWindow::socket_disconnected()
+{
+    qDebug()<<"Disconnected!";
+    ui->connecting_status->setText("Disconnected");
+}
+
+void MainWindow::on_connect_clicked()
+{
+    if (socket)
+    {
+        qDebug()<<"Already connected!";
+        ui->connecting_status->setText("Already connected");
+    } else
+    {
+        qDebug()<<"Connecting...";
+        ui->connecting_status->setText("connecting...");
+
+        socket = new QTcpSocket();
+        QString ip = ui->ip->text();
+        socket->connectToHost(ip, ui->port->text().toInt());
+
+
+        connect(socket, &QTcpSocket::connected, this, &MainWindow::socket_connected);
+        connect(socket, &QTcpSocket::readyRead, this, &MainWindow::socket_readyRead);
+        connect(socket, &QTcpSocket::bytesWritten, this, &MainWindow::socket_bytesWritten);
+        connect(socket, &QTcpSocket::disconnected, this, &MainWindow::socket_disconnected);
+    }
+
+}
+
+
+void MainWindow::on_disconnect_clicked()
+{
+    if (!socket)
+    {
+        qDebug()<<"Not connect yet!";
+    } else
+    {
+        delete socket;
+        socket = nullptr;
+        qDebug()<<"Disconnected";
+    }
+
+}
+
+
+void MainWindow::sendData(QString input){
+    if (socket)
+    {
+        socket->write(input.toUtf8());
+        qDebug()<<"Data was sent";
+    }
+}
+
--- APFinalProject_client/mainwindow.h
+++ APFinalProject_client/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpSocket>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -14,8 +15,24 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    static QTcpSocket *socket;
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    static void sendData(QString input);
+    static QString lastReceivedMessage;
+
+public slots:
+    void socket_connected();
+    void socket_bytesWritten();
+    void socket_disconnected();
+    void socket_readyRead();
+
+
+private slots:
+    void on_connect_clicked();
+
+    void on_disconnect_clicked();
+
 
 private:
     Ui::MainWindow *ui;
